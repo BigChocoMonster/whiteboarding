9a3e9f9a6adeccb33401f52e998d45813b43f020
Deblina Pattanayak <bigchocomonster@gmail.com>
2022-11-23 23:13:32 +0530

    color control + all icon set

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [selectedColor, setSelectedColor] = useState<string>("#000000");
+
   return (
-    <div className="text-center flex gap-6">
-      <div>tailwind</div>
-      <div>setup done</div>
+    <div>
+      <div className="fixed top-2/4 right-0 -translate-y-2/4 rounded-l-xl shadow p-6">
+        <div className="rounded-2xl overflow-hidden">
+          <input
+            type="color"
+            className="cursor-pointer"
+            value={selectedColor}
+            onChange={(event) => {
+              setSelectedColor(event.target.value);
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }

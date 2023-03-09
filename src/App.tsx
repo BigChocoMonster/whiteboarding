76df3fe64f3fcbd228944081486c66a621ce85e7
Deblina Pattanayak <bigchocomonster@gmail.com>
2023-03-09 22:51:01 +0530

    handling undo/redo

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   useRef,
   useState,
 } from "react";
+import { BsArrowCounterclockwise, BsArrowClockwise } from "react-icons/bs";
 
 import Color from "./Color";
 import Rectangle from "./Rectangle";
@@ -41,15 +42,21 @@ function App() {
   /**
    * maintaining states so that we can allow user to undo stuff
    */
-  const [shapeStates, setShapeStates] = useState<
+  const [shapes, setShapes] = useState<
     {
       type: typeof selected;
+      color: string;
       details: {
         originalCursorPosition: typeof originalCursorPosition;
         finalCursorPosition: typeof cursorPosition;
       };
     }[]
   >([]);
+
+  /**
+   * maintaining popped states so that we can allow user to redo stuff
+   */
+  const [poppedShapes, setPoppedShapes] = useState<typeof shapes>([]);
 
   // saves the moving cursor position
   const saveMovingCursorPosition = useCallback(
@@ -77,24 +84,125 @@ function App() {
     [saveMovingCursorPosition]
   );
 
+  // transforms from object to a string that css can understand
+  const colorHslString = useMemo(
+    () =>
+      `hsl(${selectedColor.hue}, ${Math.round(
+        selectedColor.saturation * 100
+      )}%, ${Math.round(selectedColor.lightness * 100)}%)`,
+    [selectedColor]
+  );
+
+  /**
+   * performs the actual painting on canvas depending
+   * on the shape
+   */
+  const draw = useCallback((shape: typeof shapes[number]) => {
+    if (contextRef.current) {
+      contextRef.current.beginPath();
+
+      contextRef.current.lineWidth = 1;
+      contextRef.current.strokeStyle = shape.color;
+
+      const x = shape.details.originalCursorPosition.x;
+      const y = shape.details.originalCursorPosition.y;
+      const width =
+        shape.details.finalCursorPosition.x -
+        shape.details.originalCursorPosition.x;
+      const height =
+        shape.details.finalCursorPosition.y -
+        shape.details.originalCursorPosition.y;
+
+      switch (shape.type) {
+        case "rectangle": {
+          contextRef.current.rect(x, y, width, height);
+
+          break;
+        }
+
+        case "line": {
+          contextRef.current.moveTo(
+            shape.details.originalCursorPosition.x,
+            shape.details.originalCursorPosition.y
+          );
+          contextRef.current.lineTo(
+            shape.details.finalCursorPosition.x,
+            shape.details.finalCursorPosition.y
+          );
+
+          break;
+        }
+
+        case "ellipse": {
+          const xRadius =
+            Math.abs(
+              shape.details.originalCursorPosition.x -
+                shape.details.finalCursorPosition.x
+            ) / 2;
+          const yRadius =
+            Math.abs(
+              shape.details.originalCursorPosition.y -
+                shape.details.finalCursorPosition.y
+            ) / 2;
+          const cx =
+            shape.details.originalCursorPosition.x <
+            shape.details.finalCursorPosition.x
+              ? shape.details.originalCursorPosition.x + xRadius
+              : shape.details.finalCursorPosition.x + xRadius;
+          const cy =
+            shape.details.originalCursorPosition.y <
+            shape.details.finalCursorPosition.y
+              ? shape.details.originalCursorPosition.y + yRadius
+              : shape.details.finalCursorPosition.y + yRadius;
+
+          contextRef.current.ellipse(
+            cx,
+            cy,
+            xRadius,
+            yRadius,
+            0,
+            0,
+            2 * Math.PI
+          );
+
+          break;
+        }
+
+        default: {
+          // do nothing
+        }
+      }
+
+      contextRef.current.stroke();
+    }
+  }, []);
+
   /**
    * deletes the starting cursor position
    *
    * resets the shape helper
+   *
+   * stores data in selected and popped shapes arrays
    *
    * actually helps draw the shape if shape is
    * selected
    */
   const deleteOriginalCursorPosition = useCallback(() => {
     if (selected !== "pencil") {
-      const newShape: typeof shapeStates[number] = {
+      const newShape: typeof shapes[number] = {
         type: selected,
+        color: colorHslString,
         details: {
           originalCursorPosition,
           finalCursorPosition: cursorPosition,
         },
       };
-      setShapeStates((currentStates) => currentStates.concat(newShape));
+      setShapes((currents) => currents.concat(newShape));
+      /**
+       * a new shape drawn on canvas means the user can't redo older shapes
+       * since otherwise the timeline of shapes is not be sequential
+       */
+      setPoppedShapes([]);
       draw(newShape);
 
       const helperTip = document.getElementById("shape-helper");
@@ -113,16 +221,7 @@ function App() {
 
     // @ts-ignore
     originalCursorPosition = undefined;
-  }, [selected]);
-
-  // transforms from object to a string that css can understand
-  const colorHslString = useMemo(
-    () =>
-      `hsl(${selectedColor.hue}, ${Math.round(
-        selectedColor.saturation * 100
-      )}%, ${Math.round(selectedColor.lightness * 100)}%)`,
-    [selectedColor]
-  );
+  }, [selected, colorHslString, draw]);
 
   /**
    * creates an illusion that something is being drawn but actual
@@ -234,102 +333,88 @@ function App() {
   );
 
   /**
-   * this performs the actual painting on canvas depending
-   * on the shape
+   * clears everything on the canvas
+   *
+   * draws everything that exists in the shapes array
    */
-  const draw = useCallback(
-    (shape: typeof shapeStates[number]) => {
+  const drawEverything = useCallback(
+    (redrawnShapes: typeof shapes) => {
       if (contextRef.current) {
-        contextRef.current.beginPath();
+        const canvas = document.getElementById("board") as HTMLCanvasElement;
+        contextRef.current.clearRect(0, 0, canvas.width, canvas.height);
 
-        contextRef.current.lineWidth = 1;
-        contextRef.current.strokeStyle = colorHslString;
-
-        const x = shape.details.originalCursorPosition.x;
-        const y = shape.details.originalCursorPosition.y;
-        const width =
-          shape.details.finalCursorPosition.x -
-          shape.details.originalCursorPosition.x;
-        const height =
-          shape.details.finalCursorPosition.y -
-          shape.details.originalCursorPosition.y;
-
-        switch (shape.type) {
-          case "rectangle": {
-            contextRef.current.rect(x, y, width, height);
-
-            break;
-          }
-
-          case "line": {
-            contextRef.current.moveTo(
-              shape.details.originalCursorPosition.x,
-              shape.details.originalCursorPosition.y
-            );
-            contextRef.current.lineTo(
-              shape.details.finalCursorPosition.x,
-              shape.details.finalCursorPosition.y
-            );
-
-            break;
-          }
-
-          case "ellipse": {
-            const xRadius =
-              Math.abs(
-                shape.details.originalCursorPosition.x -
-                  shape.details.finalCursorPosition.x
-              ) / 2;
-            const yRadius =
-              Math.abs(
-                shape.details.originalCursorPosition.y -
-                  shape.details.finalCursorPosition.y
-              ) / 2;
-            const cx =
-              shape.details.originalCursorPosition.x <
-              shape.details.finalCursorPosition.x
-                ? shape.details.originalCursorPosition.x + xRadius
-                : shape.details.finalCursorPosition.x + xRadius;
-            const cy =
-              shape.details.originalCursorPosition.y <
-              shape.details.finalCursorPosition.y
-                ? shape.details.originalCursorPosition.y + yRadius
-                : shape.details.finalCursorPosition.y + yRadius;
-
-            contextRef.current.ellipse(
-              cx,
-              cy,
-              xRadius,
-              yRadius,
-              0,
-              0,
-              2 * Math.PI
-            );
-
-            break;
-          }
-
-          default: {
-            // do nothing
-          }
-        }
-
-        contextRef.current.stroke();
+        redrawnShapes.forEach((shape) => {
+          draw(shape);
+        });
       }
     },
-    [colorHslString]
+    [draw]
   );
+
+  const undoShape = useCallback(() => {
+    let redrawnShapes: typeof shapes = [];
+
+    setShapes((currentShapes) => {
+      redrawnShapes = JSON.parse(JSON.stringify(currentShapes));
+
+      const removedShape = redrawnShapes.pop();
+      if (Boolean(removedShape)) {
+        /**
+         * or condition won't happen because of the wrapping if check
+         * but just keeping to avoid type error
+         */
+        setPoppedShapes((currentPoppedShapes) =>
+          currentPoppedShapes.concat(
+            removedShape || ({} as typeof shapes[number])
+          )
+        );
+      }
+
+      drawEverything(redrawnShapes);
+
+      return redrawnShapes;
+    });
+  }, [setShapes, setPoppedShapes, drawEverything]);
+
+  const redoShape = useCallback(() => {
+    let redrawnShapes: typeof shapes = [];
+
+    setPoppedShapes((currentPoppedShapes) => {
+      const clonedPoppedShapes = JSON.parse(
+        JSON.stringify(currentPoppedShapes)
+      );
+
+      const reAddedShape = clonedPoppedShapes.pop();
+      if (Boolean(reAddedShape)) {
+        /**
+         * or condition won't happen because of the wrapping if check
+         * but just keeping to avoid type error
+         */
+        setShapes((currentShapes) => {
+          redrawnShapes = currentShapes.concat(
+            reAddedShape || ({} as typeof shapes[number])
+          );
+
+          drawEverything(redrawnShapes);
+
+          return redrawnShapes;
+        });
+      }
+
+      return clonedPoppedShapes;
+    });
+  }, [setShapes, setPoppedShapes, drawEverything]);
 
   return (
     <>
-      <div className="fixed w-fit top-2/4 left-0 -translate-y-2/4 flex flex-col gap-6 select-none">
-        <div className="relative rounded-r-xl shadow-lg p-3 bg-white flex flex-col gap-4">
+      <div className="fixed w-fit top-2/4 left-4 -translate-y-2/4 flex flex-col gap-6 select-none">
+        <div className="relative rounded-xl shadow-xl hover:shadow-2xl p-3 bg-white flex flex-col gap-4">
           <Color
             selectedColor={selectedColor}
             setSelectedColor={setSelectedColor}
           />
         </div>
-        <div className="rounded-r-xl shadow-lg p-3 bg-white flex flex-col gap-4">
+        <div className="rounded-xl shadow-xl hover:shadow-2xl p-3 bg-white flex flex-col gap-4">
           <Rectangle
             isSelected={selected === "rectangle"}
             selectionColor={colorHslString}
@@ -351,6 +436,30 @@ function App() {
               setSelected("line");
             }}
           />
+        </div>
+      </div>
+      <div className="fixed w-fit left-2/4 bottom-4 -translate-x-2/4">
+        <div className="relative rounded-xl shadow-xl hover:shadow-2xl p-3 bg-white flex gap-4">
+          <button
+            disabled={shapes.length < 1}
+            className={`cursor-pointer p-2 rounded`}
+            onClick={undoShape}
+          >
+            <BsArrowCounterclockwise
+              size={24}
+              color={shapes.length < 1 ? "rgb(100, 116, 139)" : ""}
+            />
+          </button>
+          <button
+            disabled={poppedShapes.length < 1}
+            className={`cursor-pointer p-2 rounded`}
+            onClick={redoShape}
+          >
+            <BsArrowClockwise
+              size={24}
+              color={poppedShapes.length < 1 ? "rgb(100, 116, 139)" : ""}
+            />
+          </button>
         </div>
       </div>
       <canvas
